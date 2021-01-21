67892e5e8150946fc95225cad4722da9e6772884
김정근 <gogoogjg@tripstore.kr>
2021-01-21 22:14:02 +0900

    주문자 id 데이터베이스 칼럼값을 order_id로 변경한다

--- src/main/kotlin/me/kjgleh/yes25/order/command/domain/Orderer.kt
+++ src/main/kotlin/me/kjgleh/yes25/order/command/domain/Orderer.kt
@@ -1,12 +1,13 @@
 package me.kjgleh.yes25.order.command.domain
 
 import me.kjgleh.yes25.member.domain.MemberId
+import javax.persistence.AttributeOverride
 import javax.persistence.Column
 import javax.persistence.Embeddable
 
 @Embeddable
 class Orderer(
-    @Column(name = "orderer_id")
+    @AttributeOverride(name = "id", column = Column(name = "order_id"))
     val memberId: MemberId,
 
     @Column(name = "orderer_name")
