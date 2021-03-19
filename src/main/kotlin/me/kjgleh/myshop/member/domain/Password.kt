af075d4b410732b6462190017e5cd49a7e4a3f5a
김정근 <gogoogjg@tripstore.kr>
2021-03-19 15:54:44 +0900

    add initial data

--- src/main/kotlin/me/kjgleh/myshop/member/domain/Password.kt
+++ src/main/kotlin/me/kjgleh/myshop/member/domain/Password.kt
@@ -1,8 +1,10 @@
 package me.kjgleh.myshop.member.domain
 
+import javax.persistence.Column
 import javax.persistence.Embeddable
 
 @Embeddable
 class Password(
+    @Column(name = "password")
     val value: String
 )
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO myshop_member
+(member_id, name, password)
+VALUES('a0984e75-2bfc-449f-b0f6-9f1d3081d4c2', 'tester', '1f62a744-da77-498c-9ec7-3f5e3aecefc7');
+
+INSERT INTO product
+(name, price)
+VALUES('노트북', '1000000');
