168549542d10ab7ede4bcc3169617596a18bc0d7
kjg.leh <kjg.leh@gmail.com>
2022-07-15 14:21:30 +0900

    test

--- src/test/kotlin/me/kjgleh/myshop/Yes25ApplicationTests.kt
+++ src/test/kotlin/me/kjgleh/myshop/Yes25ApplicationTests.kt
@@ -2,8 +2,10 @@ package me.kjgleh.myshop
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyshopApplicationTests {
 
     @Test
